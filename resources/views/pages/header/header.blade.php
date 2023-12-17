d0fa61e84b0ffa0e75f26b98ce4b918eac088298
delaseptriani <dellasepriani256@gmail.com>
2023-12-17 22:00:57 +0700

    edit landing page

--- resources/views/pages/header/header.blade.php
+++ resources/views/pages/header/header.blade.php
@@ -8,10 +8,8 @@
         <ul>
             <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
             <li><a class="nav-link scrollto" href="#about">About</a></li>
-            <li><a class="nav-link scrollto" href="#teacher">Teacher</a></li>
             <li><a class="nav-link scrollto" href="#classroom">Classroom</a></li>
             <li><a class="nav-link scrollto" href="#team">Team</a></li>
-            <li><a class="nav-link scrollto" href="#Event">Event</a></li>
             <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
             <li><a class="getstarted scrollto" href="{{ route('login') }}">
                     @if(empty(Auth::user()->name))
